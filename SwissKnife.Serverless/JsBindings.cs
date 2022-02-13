a38e9cedd22e111c46e93fcbb3ea1dafee274136
Dmitry <d@d.d>
2022-02-13 09:28:53 +0300

    *

--- SwissKnife.Serverless/JsBindings.cs
+++ SwissKnife.Serverless/JsBindings.cs
@@ -5,6 +5,7 @@ namespace SwissKnife.Serverless;
 [JsBind("localStorage.getItem", Params = typeof((string key, int)), ReturnsNullable = typeof(string))]
 [JsBind("localStorage.setItem", Params = typeof((string key, string value)))]
 [JsBind("alert", Params = typeof((string message, int)))]
+[JsBind("prompt", Params = typeof((string message, int)), ReturnsNullable = typeof(string))]
 [JsBindingContext(JsPrefix = "BlazorBindings")]
 public static partial class JsBindings
 {
