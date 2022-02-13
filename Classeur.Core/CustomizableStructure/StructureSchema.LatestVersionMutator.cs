bdcda5e8c5838130187087a8ee2a6e246626ee8b
Dmitry <d@d.d>
2022-02-13 08:05:17 +0300

    ref

--- Classeur.Core/CustomizableStructure/StructureSchema.LatestVersionMutator.cs
+++ Classeur.Core/CustomizableStructure/StructureSchema.LatestVersionMutator.cs
@@ -28,17 +28,17 @@ public partial class StructureSchema
         /// Latest version can decrease. That's why RemoveField does not return LatestVersionMutator
         /// </remarks>
         public StructureSchema RemoveField(FieldKey key, bool preserveVersion) => preserveVersion
-            ? UpdateCurrentVersion(FieldRemoved(key, Latest.Version))
+            ? UpdateInLatestVersion(FieldRemoved(key, Latest.Version))
             : Schema.AddChange(FieldRemoved(key, Latest.NextVersion));
 
         /// <remarks>
         /// Latest version can decrease. That's why MoveField does not return LatestVersionMutator
         /// </remarks>
         public StructureSchema MoveField(FieldKey key, int position, bool preserveVersion) => preserveVersion
-            ? UpdateCurrentVersion(FieldMoved(key, position, Latest.Version))
+            ? UpdateInLatestVersion(FieldMoved(key, position, Latest.Version))
             : Schema.AddChange(FieldMoved(key, position, Latest.NextVersion));
 
-        private StructureSchema UpdateCurrentVersion(in Change change)
+        private StructureSchema UpdateInLatestVersion(in Change change)
         {
             int latestVersion = Latest.Version;
 
@@ -54,23 +54,50 @@ public partial class StructureSchema
                     Changes.Take(totalPreviousChanges))
                 .ToList();
 
-            List<FieldDescription> removed = previousFields.Except(allNewestFields).ToList();
+            CalculateDiff(from: previousFields,
+                          to: allNewestFields,
+                          targetVersion: latestVersion,
+                          removed: out List<FieldDescription> removed,
+                          added: out List<FieldDescription> added,
+                          moves: out List<Change> moves);
 
-            List<FieldDescription> added = allNewestFields.Except(previousFields).ToList();
+            return new StructureSchema(Schema.Id,
+                                       Changes.Take(totalPreviousChanges)
+                                              .Concat(removed.Select(f => FieldRemoved(f.Key, latestVersion)))
+                                              .Concat(added.Select(f => FieldAdded(f, latestVersion)))
+                                              .Concat(moves)
+                                              .ToImmutableList());
+        }
+
+        private static void CalculateDiff(List<FieldDescription> from,
+                                          List<FieldDescription> to,
+                                          int targetVersion,
+                                          out List<FieldDescription> removed,
+                                          out List<FieldDescription> added,
+                                          out List<Change> moves)
+        {
+            // Rename parameters for convenience
+            List<FieldDescription> oldFields = from;
+            List<FieldDescription> newFields = to;
+
+            removed = oldFields.Except(newFields).ToList();
+
+            added = newFields.Except(oldFields).ToList();
 
             // Contains the same set of fields as `allNewestFields` but possible moves are not applied yet
-            List<FieldDescription> fieldsToMove = previousFields.Except(removed)
-                                                                .Concat(added)
-                                                                .ToList();
+            List<FieldDescription> fieldsToMove = oldFields.Except(removed)
+                                                           .Concat(added)
+                                                           .ToList();
 
-            List<Change> moves = new(capacity: Math.Max(previousFields.Count, allNewestFields.Count));
+            moves = new List<Change>(capacity: Math.Max(oldFields.Count, newFields.Count));
 
             for (int i = 0; i < fieldsToMove.Count; ++i)
             {
-                FieldKey keyToMatch = allNewestFields[i].Key;
+                FieldKey keyToMatch = newFields[i].Key;
 
                 if (fieldsToMove[i].Key != keyToMatch)
                 {
+                    // Looking for a field to move it to position `i`
                     (int k, FieldDescription fieldToMove) = fieldsToMove.Skip(i + 1)
                                                                         .Select((f, j) => (Index: i + 1 + j, Field: f))
                                                                         .First(x => x.Field.Key == keyToMatch);
@@ -79,16 +106,9 @@ public partial class StructureSchema
                     fieldsToMove.RemoveAt(k);
                     fieldsToMove.Insert(i, fieldToMove);
 
-                    moves.Add(FieldMoved(fieldToMove.Key, position: i, latestVersion));
+                    moves.Add(FieldMoved(fieldToMove.Key, position: i, targetVersion));
                 }
             }
-
-            return new StructureSchema(Schema.Id,
-                                       Changes.Take(totalPreviousChanges)
-                                              .Concat(removed.Select(f => FieldRemoved(f.Key, latestVersion)))
-                                              .Concat(added.Select(f => FieldAdded(f, latestVersion)))
-                                              .Concat(moves)
-                                              .ToImmutableList());
         }
     }
 }
