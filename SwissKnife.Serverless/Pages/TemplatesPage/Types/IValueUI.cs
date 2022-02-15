7bc0aee0e06a1e6671863afa38947db89bcf427e
Dmitry <d@d.d>
2022-02-15 14:19:59 +0300

    validation

--- SwissKnife.Serverless/Pages/TemplatesPage/Types/IValueUI.cs
+++ SwissKnife.Serverless/Pages/TemplatesPage/Types/IValueUI.cs
@@ -1,4 +1,5 @@
-﻿using Classeur.Core.CustomizableStructure;
+﻿using System.Diagnostics.CodeAnalysis;
+using Classeur.Core.CustomizableStructure;
 using Microsoft.AspNetCore.Components;
 
 namespace SwissKnife.Serverless.Pages.TemplatesPage.Types;
@@ -7,9 +8,9 @@ public interface IValueUI
 {
     public FieldDescription Field { get; set; }
 
-    public object Value { get; set; }
+    public object Value { set; }
 
     public EventCallback Changing { get; set; }
 
-    object GetValue();
+    bool TryGetValue([NotNullWhen(returnValue: true)]out object? value);
 }
